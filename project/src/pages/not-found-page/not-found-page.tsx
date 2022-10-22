37b2ef1315980336caad434deec74e74e9c8e654
Dima Os <dimaos@protonmail.com>
2022-10-22 16:34:54 +0300

    Стиллизует стр. 404

--- project/src/pages/not-found-page/not-found-page.tsx
+++ project/src/pages/not-found-page/not-found-page.tsx
@@ -2,15 +2,17 @@ import { Link } from 'react-router-dom';
 
 import { AppRoute } from '../../const';
 
-function NotFoundPage():JSX.Element {
+import styles from './not-found-page.module.css';
+
+function NotFoundPage(): JSX.Element {
   return (
     <div className="page page--gray">
       <main className="page__main">
         <div className="container">
-          <section className="not-found">
-            <h1>404</h1>
-            <p>Page not found</p>
-            <Link to={AppRoute.Main}>Go to main page</Link>
+          <section className={styles['not-found']}>
+            <p className={styles['not-found__code']}>404</p>
+            <h1 className={styles['not-found__title']}>Page not found</h1>
+            <Link to={AppRoute.Main} className={styles['not-found__link']}>Go to main page</Link>
           </section>
         </div>
       </main>
