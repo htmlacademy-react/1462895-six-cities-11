accc2a1205a79a903d2bbd1514dc08aa8bad6b57
Dima Os <dimaos@protonmail.com>
2022-11-04 14:27:44 +0300

    Добавляет моки для предложений и комментариев

---
+++ project/src/types/offer.ts
@@ -0,0 +1,31 @@
+import { User } from './user';
+
+type location = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+};
+
+type OfferCity = {
+  location: location;
+  name: string;
+};
+
+export type Offer = {
+  bedrooms: number;
+  city: OfferCity;
+  description: string;
+  goods: string[];
+  host: User;
+  id: number;
+  images: string[];
+  isFavorite: boolean;
+  isPremium: boolean;
+  location: location;
+  maxAdults: number;
+  previewImage: string;
+  price: number;
+  rating: number;
+  title: string;
+  type: string;
+};
