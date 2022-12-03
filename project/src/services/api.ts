d038131f05ccce79bbabea4eb053adc494a03018
Dima Os <dimaos@protonmail.com>
2022-12-03 13:14:17 +0300

    Добавляет поддержку токенов

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,6 +1,7 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { getToken } from './token';
 
-const BASE_URL = '';
+const BASE_URL = 'https://11.react.pages.academy/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = ():AxiosInstance => {
@@ -8,6 +9,18 @@ export const createAPI = ():AxiosInstance => {
     baseURL: BASE_URL,
     timeout: REQUEST_TIMEOUT,
   });
+
+  api.interceptors.request.use(
+    (config: AxiosRequestConfig) => {
+      const token = getToken();
+
+      if (token && config.headers) {
+        config.headers['X-Token'] = token;
+      }
+
+      return config;
+    }
+  );
 
   return api;
 };
